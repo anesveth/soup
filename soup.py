673bf1e573b2beb9ba7e758bb8947a125e863400
anesveth <annesveth@ufm.edu>
2019-09-20 16:47:57 -0600

    made basic layout and prints title phase 1

--- soup.py
+++ soup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from bs4 import BeautifulSoup
 import requests,sys,csv,json
+import sys
 
 url="http://ufm.edu/Portal"
 # Make a GET request to fetch the raw HTML content
@@ -15,10 +16,63 @@ soup = BeautifulSoup(html_content, "html.parser")
 
 # print if needed, gets too noisy
 #print(soup.prettify())
+# for div in soup.find_all("div"):
+#     print(div)
+#     print("--------------------------")
 
-print(soup.title)
-print(soup.title.string)
 
-for div in soup.find_all("div"):
-    print(div)
-    print("--------------------------")
+# print(soup.title)
+# print(soup.title.string)
+
+
+def myname():
+    return ("""\n<Sharon Anesveth Alvarado Maatens>""")
+
+def portal():
+    return(f"""
+=============================
+1. Portal
+{soup.title.string}
+    """)
+
+
+# 1. Portal
+# # item_title: <result>
+# GET the title and print it: <result>
+# ---------------------------------------
+# GET the Complete Address of UFM: <result>
+# ------------------------------------------
+# .
+# .
+# .
+# find all properties that have href (link to somewhere):
+# - <result 1>
+# - <result 2>
+# - <result 3>
+# =============================
+# 2. Estudios
+# # ----- : separator between items
+
+# # ===== : separator between parts
+
+# # 1. Title: Title of the section
+
+# # use '-' if its a list
+
+def main():
+    choice=(sys.argv)
+    try:
+        if choice[1]=="1":
+            print(myname())
+            print(portal())
+        if choice[1]=="2":
+            pass
+        if choice[1]=="3":
+            pass
+        if choice[1]=="4":
+            pass
+    except:
+        print(myname())
+        print(portal())
+
+main()
