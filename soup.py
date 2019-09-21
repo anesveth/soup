63ff431aa3bc67a302e5654cc47008a551f1fc8a
anesveth <annesveth@ufm.edu>
2019-09-20 23:39:52 -0600

    gets items from upper nav menu

--- soup.py
+++ soup.py
@@ -16,20 +16,23 @@ soup = BeautifulSoup(html_content, "html.parser")
 
 # print if needed, gets too noisy
 #print(soup.prettify())
-# for div in soup.find_all("div"):
-#     print(div)
-#     print("--------------------------")
-
-
-# print(soup.title)
-# print(soup.title.string)
-
 
 def myname():
+    '''prints out my name'''
     return ("""\n<Sharon Anesveth Alvarado Maatens>""")
 
+def listprinting(lista):
+    '''prints out elements of a list in correct layout'''
+    l=[]
+    for i in range(len(lista)):
+        l.append(f"\n- {lista[i]}\n")
+    return l
+
 def portal():
-    ########FINDING PHONE NUMBER AND EMAIL
+    #########################################TITLE
+    title=soup.title.string
+
+    #########################################FINDING PHONE NUMBER AND EMAIL
     tagcounter=0
     listofdivs=[]
     for tag in soup.find_all("div", {"id": "footer"}):
@@ -41,27 +44,38 @@ def portal():
     listoftexts=t.split("\n")
     email=listoftexts[5].strip()
     phonenumer=listoftexts[1].strip()
-    
-        # b=b.text
-        # counter=0
-        # for a in b:
-        #     counter+=1 
-        # print(counter) 
-    ########counts a
+    #########################################GET all items that are part of the upper nav menu (id: menu-table)
+    listofnavmenuitems=[]
+    for tag in soup.find_all("table", {"id": "menu-table"}):
+        menukeys = tag.find_all("div", {"class": "menu-key"})
+        for k in menukeys:
+            k=k.text
+            listofnavmenuitems.append(k.strip("\n"))
+    for i in range(len(listofnavmenuitems)):
+        try:
+            ## gets rid of tons of unnecessary \t
+            listofnavmenuitems[i]=listofnavmenuitems[i].replace("\t","")
+        except:
+            pass
+        listofnavmenuitems[i]=(listofnavmenuitems[i].strip(" ")).strip("\n")
+    #########################################COUNTS <a>
     a=0
     for b in soup.find_all("a"):
         a+=1
+
     print(f"""
 =============================
 1. Portal
 
-GET the title and print it: {soup.title.string}
+GET the title and print it: {title}
 ---------------------------------------
 GET the complete adress of UFM: {url}
 ------------------------------------------
 GET the phone number and info email: {phonenumer}\n{email}
 ------------------------------------------
-GET all item that are part of the upper nav menu: 
+GET all items that are part of the upper nav menu:""")
+    print(*(listprinting(listofnavmenuitems)))
+    print(f"""
 ------------------------------------------
 find all properties that have href: 
 ------------------------------------------
